ab93058497b95c6adc3590fd9390ec2bd46afc2c
RyunosukeYukawa <58897653+yushe629@users.noreply.github.com>
2022-05-27 23:33:12 +0900

    Add-timer and mapdata_ndarry

--- gas_distribution/scripts/gas_distributer.py
+++ gas_distribution/scripts/gas_distributer.py
@@ -13,13 +13,14 @@ class GasDistributer:
 
         rospy.init_node("gas_distributer")
 
-        self.gas_origin = rospy.get_param("gas_origin", [0,0,0])
+        self.gas_origin = rospy.get_param("gas_origin", [2,1,0])
         self.gas_origin = np.array(self.gas_origin)
         self.alpha = rospy.get_param("~alpha", 1.0)
         self.max_val = rospy.get_param("~gap_max_val", 100)
         self.gas_value_pub = rospy.Publisher("/gas", Float32, queue_size=10)
         self.robot_pose_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
 
+        rospy.loginfo_once("gas_origin: %s", self.gas_origin)
         rospy.spin()
 
     def odom_callback(self, msg):
@@ -31,8 +32,9 @@ class GasDistributer:
         dist = np.linalg.norm(self.gas_origin - pos)
         val = self.max_val*math.exp(-dist**2)
         # val = self.max_val - self.alpha *  dist**2
-
-        self.gas_value_pub.publish(Float32(val))
+        value = Float32()
+        value.data = val
+        self.gas_value_pub.publish(value)
 
 
 if __name__ == "__main__":
--- turtlebot3_explore/scripts/gas_explore.py
+++ turtlebot3_explore/scripts/gas_explore.py
@@ -7,6 +7,8 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32
 from turtlebot3_explore.msg import PositionAndGas
 from nav_msgs.msg import Odometry
+from move_base_msgs.msg import MoveBaseActionGoal
+import numpy as np
 
 # if scan_val is Inf, inf_distance is assigned.
 inf_distance = 5.0
@@ -25,41 +27,49 @@ class gas_explore:
 
         rospy.init_node("gas_explore")
         self.vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-        # self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.callback, callback_args=0)
-        # self.gas_value_sub = rospy.Subscriber("/gas", Float32, self.callback, callback_args=1)
+        self.goal_pub = rospy.Publisher("/move_base/goal", MoveBaseActionGoal, queue_size=10)
         self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.callback)
         self.gas_value_sub = rospy.Subscriber("/gas", Float32, self.gas_callback)
         self.robot_pose_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         # self.mf = message_filters.ApproximateTimeSynchronizer([self.scan_sub, self.gas_value_sub], queue_size=10, deley =1/100. * 0.5)
         # self.mf.registerCallback(self.callback)
-        # self.maxConcentrate = 0
         self.before_gas_value = 0.0
         self.gas_value = 0.0
-        self.robot_position
+        self.max_gas_value = 0.0
+        self.robot_position = None
         self.cmd_x = 0.0
         self.cmd_yaw = 0.0
         self.explore_state = 'front'
         self.iscorrect_path = True
-        self.maxValue = 0.0
-        self.setMaxValueTime = rospy.Time.now()
-        self.setMaxValueOdom
+        self.max_gas_value_time = rospy.Time()
+        self.begin_time = rospy.Time.now()
+        self.max_gas_value_position = None
+        self.gas_map_array = np.empty((0,3), np.float32)
         rospy.spin()
         # TODO How to use two or three subscribers
+    
+    def time_now_in_node(self):
+        t = rospy.Time.now()
+        return t - self.begin_time
 
     def odom_callback(self,msg):
         # set Odometry.pose.pose.position
         self.robot_position = msg.pose.pose.position
-        
+
     def gas_callback(self, msg):
         self.before_gas_value = self.gas_value
-        self.gas_value = msg
-        if msg > self.maxValue:
-            self.maxValue = msg
-            self.setMaxValueTime = rospy.Time.now()
-            self.maxvalueOdom = self.robot_position
-            
+        self.gas_value = msg.data
+        # Add gas map date to gas_map_array
+        if self.robot_position != None:
+            self.gas_map_array = np.append(self.gas_map_array, [self.robot_position.x, self.robot_position.y, msg.data], axis=0)
+
+        if msg.data > self.max_gas_value:
+            self.max_gas_value = msg.data
+            self.max_gas_value_time = self.time_now_in_node()
+            self.max_gas_value_position = self.robot_position
+
     def explore(self):
-        if self.explore_state =='front':
+        if self.explore_state == 'front':
             self.cmd_x = explore_vel
             self.cmd_yaw = 0.0
             self.explore_state = 'back'
@@ -79,12 +89,19 @@ class gas_explore:
             self.explore_state == 'front'
 
     def callback(self, msg):
-        rospy.loginfo_throttle(1.0, "laser number: %d", len(msg.ranges))
-        if ((self.setMaxValueTime - rospy.Time.now()).to_sec() > limit_time):
-            self.cmd_x = 0.0
-            self.cmd_yaw = 0.0
-            # TODO: set goal as self.maxValueOdom
-            # use diff or move_base
+        # rospy.loginfo_throttle(1.0, "laser number: %d", len(msg.ranges))
+        last_sec = (self.time_now_in_node().to_sec() - self.max_gas_value_time.to_sec())
+        rospy.loginfo_throttle(1.0,"last_time: %f",last_sec)
+        # if (last_sec  > limit_time):
+        #     rospy.loginfo_once("Robot discovered goal")
+        #     self.explore_state = "explored"
+        #     self.cmd_x = 0.0
+        #     self.cmd_yaw = 0.0
+        #     goal = MoveBaseActionGoal()
+        #     goal.goal.target_pose.pose.position = self.robot_position
+        #     self.goal_pub.publish(goal)
+        #     return
+
         #front_dist = msg.ranges[0]
         front_dists = msg.ranges[:20]+ msg.ranges[340:]
         front_dist = min(list(map(lambda x: inf_distance if x == float('inf') else x, front_dists)))
@@ -94,7 +111,6 @@ class gas_explore:
 
         # 180 (back):  msg.ranges[len(msg.ranges)/2]
 
-        ## TODO: please implement gas_explore action
         if (front_dist < territory_radius):
             self.cmd_x = 0.0
             self.cmd_yaw = 0.5
@@ -103,19 +119,22 @@ class gas_explore:
         else:
             self.cmd_yaw = 0.0
             self.cmd_x = 0.0
-            self.explore()
             rospy.loginfo_throttle(1.0, 'state: %s', self.explore_state)
+            self.explore()
 
 
         cmd_msg = Twist()
         cmd_msg.linear.x = self.cmd_x
         cmd_msg.angular.z =self.cmd_yaw
-        rospy.loginfo_throttle(1.0, "x: %f", self.cmd_x)
-        rospy.loginfo_throttle(1.0, "yaw: %f", self.cmd_yaw)
+        # rospy.loginfo_throttle(1.0, "value: %s", str(self.gas_value))
+        # rospy.loginfo_throttle(1.0, "before_gas_value: %s", str(self.before_gas_value))
+        # rospy.loginfo_throttle(1.0, "maxvalue: %s", str(self.max_gas_value))
+        # rospy.loginfo_throttle(1.0, "x: %f", self.cmd_x)
+        # rospy.loginfo_throttle(1.0, "yaw: %f", self.cmd_yaw)
+        # rospy.loginfo_throttle(1.0, "array: %s", self.gas_map_array)
         self.vel_pub.publish(cmd_msg)
 
 if __name__ == "__main__":
     try:
         gas_explore_action = gas_explore()
     except rospy.ROSInterruptException: pass
-
