ee27dc150d45dccd8322371ff84bd90baec285da
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2022-05-28 23:50:34 +0900

    Visualize the estimated gas map

--- turtlebot3_explore/scripts/gas_explore.py
+++ turtlebot3_explore/scripts/gas_explore.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Float32
 from turtlebot3_explore.msg import PositionAndGas
 from nav_msgs.msg import Odometry
 from move_base_msgs.msg import MoveBaseActionGoal
+from nav_msgs.msg import OccupancyGrid
 import numpy as np
 
 # if scan_val is Inf, inf_distance is assigned.
@@ -29,6 +30,7 @@ class gas_explore:
         rospy.init_node("gas_explore")
         self.vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.goal_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=10)
+        self.gas_map_pub= rospy.Publisher("/estimated_gas_map", OccupancyGrid, queue_size=1)
         self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.callback)
         self.gas_value_sub = rospy.Subscriber("/gas", Float32, self.gas_callback)
         self.robot_pose_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
@@ -45,7 +47,26 @@ class gas_explore:
         self.max_gas_value_time = rospy.Time()
         self.begin_time = rospy.Time.now()
         self.max_gas_value_pose = None
+
+        # gas map for exploreation rule
         self.gas_map_array = np.empty((0,3), np.float32)
+        # gap map for visualization
+        self.gas_visual_map = OccupancyGrid()
+        self.gas_visual_map.header.frame_id = "map"
+        self.gas_scale = rospy.get_param("~gas_visual_scale", 1.0)
+        self.gas_offset = rospy.get_param("~gas_visual_offset", 0.0)
+        self.gas_visual_map.info.resolution = rospy.get_param("~gas_visual_map_resolution", 0.05) # 0.05 m/pixel
+        origin = rospy.get_param("~gas_visual_map_origin", [-10.0, -10.0, 0.0])
+        print(origin)
+        self.gas_visual_map.info.width = int(math.fabs(origin[0]) / self.gas_visual_map.info.resolution * 2)
+        self.gas_visual_map.info.height = int(math.fabs(origin[1]) / self.gas_visual_map.info.resolution * 2)
+        self.gas_visual_map.info.origin.position.x = origin[0]
+        self.gas_visual_map.info.origin.position.y = origin[1]
+        self.gas_visual_map.info.origin.position.z = origin[2]
+        self.gas_visual_map.info.origin.orientation.w = 1
+        self.gas_visual_map.data = [0] * self.gas_visual_map.info.width * self.gas_visual_map.info.height
+        self.gas_map_pub_time = rospy.get_time()
+
         rospy.spin()
         # TODO How to use two or three subscribers
     
@@ -63,6 +84,15 @@ class gas_explore:
         # Add gas map date to gas_map_array
         if self.robot_pose != None:
             self.gas_map_array = np.append(self.gas_map_array, [[self.robot_pose.position.x, self.robot_pose.position.y, msg.data]], axis=0)
+
+            # visualize the gas map
+            pixel_x = int((self.robot_pose.position.x - self.gas_visual_map.info.origin.position.x) / self.gas_visual_map.info.resolution)
+            pixel_y = int((self.robot_pose.position.y - self.gas_visual_map.info.origin.position.y) / self.gas_visual_map.info.resolution)
+            self.gas_visual_map.data[pixel_y * self.gas_visual_map.info.width + pixel_x] = int(msg.data * self.gas_scale + self.gas_offset)
+
+            if rospy.get_time() - self.gas_map_pub_time > 1.0: # publish in 1 Hz
+                self.gas_map_pub.publish(self.gas_visual_map)
+                self.gas_map_pub_time= rospy.get_time()
 
         if msg.data > self.max_gas_value:
             self.max_gas_value = msg.data
