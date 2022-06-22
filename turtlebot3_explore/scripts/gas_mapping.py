8bdda1906c23d7dc9156bfba27b65b01ca4a440b
yushe629 <58897653+yushe629@users.noreply.github.com>
2022-06-22 13:46:48 +0900

    refactoring gas_mapping

--- turtlebot3_explore/scripts/gas_mapping.py
+++ turtlebot3_explore/scripts/gas_mapping.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
-import imp
 import rospy
 import message_filters
 import math
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist, PoseStamped
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
-from move_base_msgs.msg import MoveBaseActionGoal
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
-
-# if scan_val is Inf, inf_distance is assigned.
-inf_distance = 5.0
-# the radius of the robot explore area
-territory_radius = 0.6
-# the velocity of exploring and the type of explore state
-explore_vel = 0.3
-explore_time = 0.3
-explore_yaw_vel = 1.0
-explore_yaw_time = 0.3
-explore_state = ['front', 'back', 'turn', 'after_turn', 'explored']
-limit_time = 30.0
 
 class gas_mapping:
     def __init__(self):
@@ -32,22 +16,8 @@ class gas_mapping:
         self.robot_pose_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         # self.mf = message_filters.ApproximateTimeSynchronizer([self.scan_sub, self.gas_value_sub], queue_size=10, deley =1/100. * 0.5)
         # self.mf.registerCallback(self.callback)
-        self.before_gas_value = 0.0
-        self.gas_value = 0.0
-        self.max_gas_value = 0.0
-        self.goal_pose = PoseStamped()
-        self.goal_pose.header.frame_id = "map"
         self.robot_pose = None
-        self.cmd_x = 0.0
-        self.cmd_yaw = 0.0
-        self.explore_state = 'front'
-        self.iscorrect_path = True
-        self.max_gas_value_time = rospy.Time()
-        self.begin_time = rospy.Time.now()
-        self.max_gas_value_pose = None
 
-        # gas map for exploreation rule
-        self.gas_map_array = np.empty((0,3), np.float32)
         # gap map for visualization
         self.gas_visual_map = OccupancyGrid()
         self.gas_visual_map.header.frame_id = "map"
@@ -69,20 +39,13 @@ class gas_mapping:
         rospy.spin()
         # TODO How to use two or three subscribers
 
-    def time_now_in_node(self):
-        t = rospy.Time.now()
-        return t - self.begin_time
-
     def odom_callback(self,msg):
         # set Odometry.pose.pose
         self.robot_pose = msg.pose.pose
 
     def gas_callback(self, msg):
-        self.before_gas_value = self.gas_value
-        self.gas_value = msg.data
         # Add gas map date to gas_map_array
         if self.robot_pose != None:
-            self.gas_map_array = np.append(self.gas_map_array, [[self.robot_pose.position.x, self.robot_pose.position.y, msg.data]], axis=0)
 
             # visualize the gas map
             pixel_x = int((self.robot_pose.position.x - self.gas_visual_map.info.origin.position.x) / self.gas_visual_map.info.resolution)
@@ -92,11 +55,6 @@ class gas_mapping:
             if rospy.get_time() - self.gas_map_pub_time > 1.0: # publish in 1 Hz
                 self.gas_map_pub.publish(self.gas_visual_map)
                 self.gas_map_pub_time= rospy.get_time()
-
-        if msg.data > self.max_gas_value:
-            self.max_gas_value = msg.data
-            self.max_gas_value_time = self.time_now_in_node()
-            self.max_gas_value_pose = self.robot_pose
 
 if __name__ == "__main__":
     try:
